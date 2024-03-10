bb256f7057e744f2b458d2d5fec8380e82b207bf
iihll <iihll.dev@gmail.com>
2024-03-10 09:58:29 +0800

    feat: Row Col

--- packages/docs/src/pages/index/index.vue
+++ packages/docs/src/pages/index/index.vue
@@ -4,44 +4,14 @@ import { Button } from 'uni-vant'
 
 <template>
   <view class="content">
-    <view class="text-area">
-      <Button type="primary" size="small" loading loading-text="加载中..." loading-type="spinner">
-        123
-      </Button>
-      <Button type="primary">
-        test
-      </Button>
-      <Button icon="plus" type="primary">
-        按钮
-      </Button>
-    </view>
+    <Button type="primary" size="small" loading loading-text="加载中..." loading-type="spinner">
+      123
+    </Button>
+    <Button type="primary">
+      test
+    </Button>
+    <Button icon="plus" type="warning">
+      按钮
+    </Button>
   </view>
 </template>
-
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
