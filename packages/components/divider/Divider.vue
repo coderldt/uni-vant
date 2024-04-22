41ff6e4e29149e98745747727448b1deac4dbd47
吕超 <1969956387@qq.com>
2024-04-22 16:49:35 +0800

    feat: add divider component

---
+++ packages/components/divider/Divider.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import './index.less'
+import { useSlots } from 'vue'
+import { createNamespace } from '../utils'
+import { dividerProps } from './types'
+
+const props = defineProps(dividerProps)
+
+const [name, bem] = createNamespace('divider')
+
+const slots = useSlots()
+</script>
+
+<template>
+  <div
+    role="separator"
+    :class="bem({
+      dashed: props.dashed,
+      hairline: props.hairline,
+      vertical: props.vertical,
+      [`content-${props.contentPosition}`]:
+        !!slots.default && !props.vertical,
+    })"
+  >
+    <slot v-if="!props.vertical"></slot>
+  </div>
+</template>
